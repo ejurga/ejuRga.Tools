6abac3808045f564f652209795a29e034014547f
ejurga <emiljurga@hotmail.com>
2020-03-13 15:17:42 -0400

    Removed the filter_dups function, as this is better implemented in the janitor R package

--- R/functions.R
+++ R/functions.R
@@ -19,28 +19,6 @@ dil = function(){
     .rs.restartR()
 }
 
-#' Return dataframe containing duplicate values
-#'
-#' Uses tidyeval. This function takes a dataframe and a column in the dataframe, and
-#' returns the dataframe filtered for duplicate values in the specified column.
-#'
-#' @usage filter_dups(df, var, n=1)
-#'
-#' @param df Dataframe
-#' @param var The column name for which to filter the duplicates
-#' @param n Return rows containing more then n instances of the specified column.
-#'
-#' @return A dataframe containing the rows filtered for duplicates in the specified
-#' column.
-#'
-#' @export
-filter_dups = function(df, var, n=1) {
-    var = enquo(var)
-    dups = df %>% group_by(!!var) %>% tally() %>% filter(n>n) %>% pull(!!var)
-    df.filt = df %>% filter(!!var %in% dups) %>% arrange(!!var)
-    return(df.filt)
-}
-
 #' Return named vector of 20 custom colors
 #'
 #' Use this function to return a custom named list of 20 colors. These are somewhat
@@ -76,32 +54,32 @@ twentycol = function() {
 }
 
 #' Save a figure as a pdf
-#' 
-#' This function exists as a convienient wrapper around the usual workflow of 
-#' saving a ggplot figure as a pdf. The dimensions of the pdf are wrapped into 
-#' keywords. The path is handled automatically, as I usually save figures in 
+#'
+#' This function exists as a convienient wrapper around the usual workflow of
+#' saving a ggplot figure as a pdf. The dimensions of the pdf are wrapped into
+#' keywords. The path is handled automatically, as I usually save figures in
 #' the same directory structure for each project. And a convinient option exists
-#' to check for the existence of the pdf and not generate it if specified. 
-#' 
-#' @usage save_figure(fig, size='sixth', filename='scratch.pdf', 
+#' to check for the existence of the pdf and not generate it if specified.
+#'
+#' @usage save_figure(fig, size='sixth', filename='scratch.pdf',
 #'        dir='results/figures/', margins=1, units='in', regen=TRUE)
 #'
 #' @param fig The ggplot object to save
-#' @param size The size of the pdf. Takes one of 'full', 'half', 'quarter', 'third', 
+#' @param size The size of the pdf. Takes one of 'full', 'half', 'quarter', 'third',
 #' 	or 'sixth'. Alternatively, supply a vector of lencth 2, as c('height', 'width').
-#' \describe{ 
+#' \describe{
 #' 	\item{full}{Full page}
 #' 	\item{half}{Half of the page, folded height-wise}
 #' 	\item{quarter}{One quarter of the page, folded height and width-wise}
 #' 	\item{third}{One third of the page, folded height-wise}
-#' 	\item{sixth}{One sixth of the page, folded in half width-wise, and in a 
+#' 	\item{sixth}{One sixth of the page, folded in half width-wise, and in a
 #'		       third height-wise}
 #'}
 #' @param filename Name of the file. Include subdirectories.
 #' @param dir The default directory to save the files in
-#' @param margins The margins of the main file to which the figure is intended to be 
+#' @param margins The margins of the main file to which the figure is intended to be
 #'        saved.
-#' @param regen Whether or not to regenerate the figure on each source, given that the 
+#' @param regen Whether or not to regenerate the figure on each source, given that the
 #'        figure already exists. Beware of this option, use only on very large figures.
 #'
 #' @return Nothing, but save the figure as a pdf.
