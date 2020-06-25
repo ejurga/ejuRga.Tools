4cc13e88c8b2480cb06573a104aca5e63dd9eb62
ejurga <emiljurga@hotmail.com>
2020-06-24 20:10:09 -0400

    Added some quick load things for devtools

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,19 @@
+#' Quickly load the Rfunctions package
+#'
+#' Load the package at 'bin/Rfunctions' 
+quick_load = function(){ devtools::load_all('bin/Rfunctions') }
+
+#' Install the Rfunctions package
+#'
+#' Shortcut to document and install the R package at 'bin/Rfunction'. 
+#' Also, reload Rstudio.
+quick_install = function(){ 
+    devtools::document('bin/Rfunctions/')
+    devtools::install('bin/Rfunctions') 
+    .rs.restartR()
+}
+
+
 #' Quickly print inf rows
 #'
 #' @export
